495f17a264ad632a59693915d673e35d1b5ada78
non4me <vladimir.troyanenko@gmail.com>
2025-09-21 00:16:15 +0200

    Refactored `WidgetComponent` ticket handling: - Added `newTicket` initialization and temporary `add` method for UI support. - Improved ticket deletion logic with detailed filtering and formatting updates. - Reordered imports and removed redundancies.

--- ng18/src/app/components/widget/widget.component.ts
+++ ng18/src/app/components/widget/widget.component.ts
@@ -1,13 +1,13 @@
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {Component, inject} from '@angular/core';
+import {catchError, EMPTY, take} from "rxjs";
 import {CommonModule} from "@angular/common";
 import {TableModule} from "primeng/table";
+import {Button} from 'primeng/button';
 
 import {Ticket, CustomTicket} from '../../model/ticket';
 import {TicketService} from '../../services/ticket.service';
 import {SafeHtmlPipe} from '../../shared/pipes/safe-html.pipe';
-import {Button} from 'primeng/button';
-import {catchError, EMPTY, take} from "rxjs";
 
 @Component({
   selector: 'app-widget',
@@ -18,18 +18,21 @@ import {catchError, EMPTY, take} from "rxjs";
 })
 export class WidgetComponent {
   tickets!: CustomTicket[];
-  pendingAdd = false
+  newTicket!: CustomTicket;
 
   private dataService = inject(TicketService);
 
   constructor() {
     this.dataService.getList()
       .pipe(takeUntilDestroyed())
-      .subscribe(data => this.tickets = [...data] as CustomTicket[])
+      .subscribe(data => {
+        this.tickets = [...data] as CustomTicket[];
+        this.newTicket = this.tickets[0];
+      })
   }
 
   deleteTicket(ticket: CustomTicket): void {
-    if(!ticket.deletePending) {
+    if (!ticket.deletePending) {
       ticket.deletePending = true;
       this.dataService.deleteTicket(ticket)
         .pipe(
@@ -38,17 +41,26 @@ export class WidgetComponent {
             // pouze pro demo, ala mock
             this.tickets = [...this.tickets.filter(item => item !== ticket)];
 
-            return  EMPTY;
+            return EMPTY;
           })
         )
         .subscribe(() => {
           ticket.deletePending = false;
-          this.tickets = [...this.tickets.filter(item => item !== ticket)]
+          this.tickets = [...this.tickets.filter(item => item !== ticket)];
         })
     }
   }
 
   add(): void {
-    console.log('add')
+    // TODO: Potřebuji specifikaci pro <tsm-ticket-lov>
+    // dočasné řešení pouze pro fungování UI
+    const newTicket = {
+      ...this.newTicket,
+      id: '' + Math.random(),
+      key: 'TICK_' + Date.now(),
+      description: '' + Math.random()
+    }
+
+    this.tickets = [...this.tickets, newTicket]
   }
 }
